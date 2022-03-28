d5113bd481c7b6dc1c157166127a7615498b3298
iaraszuir <iaraszuir@gmail.com>
2022-03-28 10:41:11 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//Fichero que contienen todos los manejadores de la aplicacion
+//Mucho mas eficiente para el tema de las pruebas
+const express = require('express');
+
+//Crear la APP
+const app = express();
+
+//TODO:Configuracion de los manejadores
+
+
+//La preparamps para exportarla
+module.exports = app
---
+++ bin/server.js
@@ -0,0 +1,13 @@
+//Configuracion del servidor
+const http = require('http');
+const app = require('../app');
+
+//Configurar .env:
+require('dotenv').config();
+
+const server = http.createServer(app);
+
+let PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor escuchando en puerto ${PORT}`)
+})
